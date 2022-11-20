2b79a2133c8c2fb2dd223578cf9635f54996fa3e
mohaemmi <115582940+mohaemmi@users.noreply.github.com>
2022-11-20 14:09:26 +0100

    Initial commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public float Speed = 5.0f;
+
+private Vector3 MoveVector = Vectore3.zero;
+
+public class CameraControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
